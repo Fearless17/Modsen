5d1ead93eff2e25a344bc271cdf573c63975f4fd
Fearless17 <375336389408wlad@gmail.com>
2023-04-11 20:25:34 +0300

    JS

---
+++ js/questions.js
@@ -0,0 +1,11 @@
+let questions = [
+    {
+        ques_numb: 1,
+        question: "How many planets are in the solar system?",
+        options: [
+            "8",
+            "9",
+            "10"
+        ]
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,1 @@
+const quiz = document.querySelector(".quiz");
