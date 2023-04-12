a28434cf7b9fb1f03e07ff2504aef074d3672be3
Fearless17 <375336389408wlad@gmail.com>
2023-04-12 22:15:21 +0300

    добавил вопросы

--- js/questions.js
+++ js/questions.js
@@ -2,10 +2,78 @@ let questions = [
     {
         ques_numb: 1,
         question: "How many planets are in the solar system?",
+        answer: "8",
         options: [
             "8",
             "9",
             "10"
         ]
+           
+    },
+    {
+        ques_numb: 2,
+        question: "What is the freezing point of water?",
+        answer: "0",
+        options: [
+            "0",
+            "-5",
+            "6"
+        ]
+           
+    },
+    {
+        ques_numb: 3,
+        question: "What is the longest river in the world?",
+        answer: "Amazon",
+        options: [
+            "Nile",
+            "Amazon",
+            "Yangtze"
+        ]
+           
+    },
+    {
+        ques_numb: 4,
+        question: "How many chromosomes are in the human genome?",
+        answer: "46",
+        options: [
+            "42",
+            "44",
+            "46"
+        ]
+           
+    },
+    {
+        ques_numb: 5,
+        question: "Which of these characters are friends with Harry Potter?",
+        answer: ["Ron Weasley", "Hermione Granger"], 
+        options: [
+            "Ron Weasley",
+            "Draco Malfoy",
+            "Hermione Granger"
+        ]
+           
+    },
+    {
+        ques_numb: 6,
+        question: "What is the capital of Canada?",
+        answer: "Ottawa",
+        options: [
+            "Toronto",
+            "Ottawa",
+            "Vancouver"
+        ]
+           
+    },
+    {
+        ques_numb: 7,
+        question: "What is the Jewish New Year called?",
+        answer: "Rosh Hashanah",
+        options: [
+            "Rosh Hashanah",
+            "Yom Kippur",
+            "Kwanzaa"
+        ]
+           
     }
 ]
